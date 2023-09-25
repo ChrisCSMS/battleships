a6973a75e66ae1dff4b7e729e255ec4526933d3e
ChrisCSMS <christophermcsherry2@gmail.com>
2023-09-25 21:40:39 +0100

    Initial commit, Add Board class

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+class Board:
+    empty_location = "-"
+    hit_location = "*"
+    miss_location = "X"
+    ship_location = "@"
+    def __init__(self):
+        self.grid = [ [Board.empty_location]*5 for i in range(5)]
+
+    def add_random_ships(self, no_of_ships):
+        ships_left = no_of_ships
+        while ships_left > 0:
+
+            x = random.randint(0, len(self.grid)-1)
+            y = random.randint(0, len(self.grid)-1)
+            print(x, y)
+            if self.grid[x][y] == Board.empty_location:
+                self.grid[x][y] = Board.ship_location
+                ships_left -= 1
+
+board = Board()
+board.add_random_ships(4)
+
