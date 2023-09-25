f82aec63f8bb8eb87dad32028192450a5dbce758
ChrisCSMS <christophermcsherry2@gmail.com>
2023-09-25 22:14:54 +0100

    Add display for board

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ class Board:
     hit_location = "*"
     miss_location = "X"
     ship_location = "@"
-    def __init__(self):
-        self.grid = [ [Board.empty_location]*5 for i in range(5)]
+    def __init__(self, board_size):
+        self.grid = [ [Board.empty_location]*board_size for i in range(board_size)]
 
     def add_random_ships(self, no_of_ships):
         ships_left = no_of_ships
@@ -13,11 +13,24 @@ class Board:
 
             x = random.randint(0, len(self.grid)-1)
             y = random.randint(0, len(self.grid)-1)
-            print(x, y)
+            
             if self.grid[x][y] == Board.empty_location:
                 self.grid[x][y] = Board.ship_location
                 ships_left -= 1
 
-board = Board()
+
+    
+    def display_board(self, hide_ships):
+        for row in self.grid:
+            for col in row:
+                if hide_ships:
+                    col = col.replace(board.ship_location, board.empty_location)
+                print(col, end=" ")
+            print()
+
+
+board = Board(5)
 board.add_random_ships(4)
+board.display_board(False)
+
 
